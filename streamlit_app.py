98e17d7a4fc055ab0c135ec48dfd7c9eb88d190b
RaviSaini13 <137437004+RaviSaini13@users.noreply.github.com>
2023-06-22 23:53:56 +0530

    Update streamlit_app.py
    Added header and text to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Hello World ! First Streamlit App')
+streamlit.header('Application Menu')
+streamlit.text('Learn Snowflake')
+streamlit.text('Create GitHub Account')
+streamlit.text('Create Streanlit Account through GitHub')
+streamlit.text('Provide Access to Streamlit')
